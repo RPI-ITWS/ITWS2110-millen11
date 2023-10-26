a788f9115670fcf7757564ec39eadff39e6c93bf
millen11 <millen11@rpi.edu>
2023-10-25 21:16:21 -0400

    millen11-optimization2
    Added DNS minimization techniques like:
    * Prefetching
    * defer
    * putting script tag at end of body

--- labs/lab5/README.md
+++ labs/lab5/README.md
@@ -1,7 +1,24 @@
 Resources:
 
 1. https://www.keycdn.com/support/reduce-dns-lookups
+2. https://dev.to/ekafyi/lazy-loading-images-with-vanilla-javascript-2fbj
+3. 1x1 pmg: https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png
+4. https://www.keycdn.com/support/defer-parsing-of-javascript
 
-Design Choices:
+Design Choices & Optimizations:
 
-Issues:
+1. Moved JS and CSS code from index.html to separate files to
+
+- Follow sepatation of concerns
+
+2. Minimize DNS Lookups
+   a) Prefetch links and images \ May make navigation/viewing of certain content quicker for users
+
+   - Image lazy loading to reduce resource usage
+     b) Move script tag to bottom of body tag and add defer attribute
+
+   * Did consider a manual defer parsing technique, however I believe it may overcomplicate a pre-built method
+
+   Issues:
+
+3. "Flattening CNAME record". I tried minimizing the CNAME records, but got stonewalled by CloudFlare due to permissions and subscription model.
--- labs/lab5/freebee.js
+++ labs/lab5/freebee.js
@@ -1,3 +1,36 @@
+// https://dev.to/ekafyi/lazy-loading-images-with-vanilla-javascript-2fbj
+//Lazy loads comb images
+// Run after the HTML document has finished loading
+document.addEventListener("DOMContentLoaded", function () {
+  // Get our lazy-loaded images
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazyLoad"));
+
+  // Do this only if IntersectionObserver is supported
+  if ("IntersectionObserver" in window) {
+    // Create new observer object
+    let lazyImageObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      // Loop through IntersectionObserverEntry objects
+      entries.forEach(function (entry) {
+        // Do these if the target intersects with the root
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    // Loop through and observe each image
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
+});
+
 var all;
 var daily_play;
 var found;
